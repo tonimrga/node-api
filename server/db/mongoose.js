00eba5426f1f022517f10a2e3c82f1709654347d
toni <toni.margan@gmail.com>
2018-04-23 10:37:19 +0200

    za heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://tonimrga:bela6788@ds155299.mlab.com:55299/notes');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -44,8 +45,8 @@ app.get('/todos/:id', (req, res)=>{
     }).catch((e)=>res.status(400).send());
 });
 
-app.listen(3000, () =>{
-  console.log('server started');
+app.listen(port, () =>{
+  console.log(`server started at port ${port}`);
 });
 
 module.exports = {app};
