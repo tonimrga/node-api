734e3cad5f05b290d339191e52eab9e2b8622885
toni <toni.margan@gmail.com>
2018-04-23 11:21:34 +0200

    delete todos by id

---
+++ playground/mongoose-rermove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+//Todo.remove({}).then((result)=>{
+//    console.log(result);
+// });
+
+//Todo.findOneAndRemove
+
+//Todo.findByIdAndRemove
+
+//Todo.findByIdAndRemove('id').then((todo)=>{
+//    console.log(todo);
+//});
