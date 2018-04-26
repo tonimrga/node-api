c1e83c6d53f33194abe735a89ff765f0e4a313fe
toni <toni.margan@gmail.com>
2018-04-26 15:26:18 +0200

    private routes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 //import ObjectID modula iz mongusa koji nam omogućuje manipuliranje idom
 const {ObjectID} = require('mongodb');
@@ -116,6 +117,12 @@ app.post('/users', (req,res)=>{
   }).catch((e)=>{
     res.status(400).send(e);
   })
+  });
+
+
+
+  app.get('/users/me', authenticate, (req, res)=>{
+    res.send(req.user);
   });
 
 //pokreće server i sluša ga na portu koji je definiran gore
